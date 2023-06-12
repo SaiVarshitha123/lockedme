71b766f558b37fb7a6b2478de0a8501ad4f42b87
SaiVarshitha123 <135735200+SaiVarshitha123@users.noreply.github.com>
2023-06-12 08:10:35 +0530

    Add files via upload

---
+++ eclipse-workspace/lock/src/ascending/Ascending.java
@@ -0,0 +1,21 @@
+package ascending;
+
+import java.io.*;
+import java.util.*;
+public class Ascending {
+
+    static String directory= "/Users/saanagar/Downloads/java" ;
+    public static void ascendingOrder() {
+
+        File[] files = new File(directory).listFiles();
+        Set<String> a = new TreeSet<>();
+        for(File file : files) {
+            if (!file.isFile()) {
+                continue;
+            }
+            a.add(file.getName());
+        }
+        a.forEach(i->System.out.println(i));
+
+    }
+}
---
+++ eclipse-workspace/lock/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author saanagar
+ *
+ */
+module lock {
+}
